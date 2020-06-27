4e01c4c6c02487c94c12ad042006ad1ab8651ad5
Amit Gupta <amit_rashi@hotmail.com>
2020-06-27 17:51:18 +0100

    Added Setup File

--- Setup.sh
+++ Setup.sh
@@ -1,3 +1,4 @@
+docker network create dctm-dev
 source documentum-environment.profile
 docker-compose -f CS-Docker-Compose_Stateless.yml up -d
 docker exec postgres su -c "mkdir /var/lib/postgresql/data/db_documentum_dat.dat" postgres
