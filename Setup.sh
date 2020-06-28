d2209f85096fa4a5d80534c2c5a236996c200e0f
Amit Gupta <amit_rashi@hotmail.com>
2020-06-28 19:38:22 +0100

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 cd /home/ubuntu/DCTM-xCP-16.4.0
 sudo docker network create dctm-dev
 source documentum-environment.profile
-sudo docker-compose -f CS-Docker-Compose_Stateless.yml up -d
+sudo -E docker-compose -f CS-Docker-Compose_Stateless.yml up -d
 sleep 2s
 sudo docker exec postgres su -c "mkdir /var/lib/postgresql/data/db_documentum_dat.dat" postgres
 sudo docker ps
