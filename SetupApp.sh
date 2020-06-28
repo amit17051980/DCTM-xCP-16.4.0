6363c198b0c0a06f4bec9f6e723635be5e3db49a
Amit Gupta <amit_rashi@hotmail.com>
2020-06-28 23:32:43 +0100

    Update SetupApp.sh

--- SetupApp.sh
+++ SetupApp.sh
@@ -2,11 +2,10 @@
 cd /home/ubuntu/DCTM-xCP-16.4.0/media-files
 sudo docker run --network dctm-dev -d --name documentum-xcp --hostname documentum-xcp -p 8000:8080 amit17051980/dctm-tomcat:latest
 sleep 3s
-sudo docker stop documentum-xcp
 sudo docker cp context.xml documentum-xcp:/usr/local/tomcat/webapps/manager/META-INF/
 sudo docker cp catalina.properties documentum-xcp:/usr/local/tomcat/conf/
 sudo docker cp tomcat-users.xml documentum-xcp:/usr/local/tomcat/conf/
 sudo docker cp custom-conf documentum-xcp:/usr/local/tomcat/
-sudo docker start documentum-xcp
+sudo docker restart documentum-xcp
 sleep 3s
 sudo docker exec documentum-xcp su -c "cp -r /usr/local/tomcat/webapps.dist/manager /usr/local/tomcat/webapps/"
